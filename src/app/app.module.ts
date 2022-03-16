32e284e60b2a3130945f72f71a7e731842e888a9
Juan Carlos Perez Angeles <juan-carlos-perez-@outlook.com>
2022-03-16 17:06:32 -0600

    Creación del módulo de Reservación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import {AppRoutingModule} from "./app-routing.module";
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {LoginComponent} from "./login/login.component";
+import { ReservacionComponent } from './reservacion/reservacion.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ReservacionComponent
   ],
   imports: [
     BrowserModule,
