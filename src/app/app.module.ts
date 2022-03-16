b830f849f26f46885bb361563896aa63dbcec81e
Juan Carlos Perez Angeles <juan-carlos-perez-@outlook.com>
2022-03-16 15:13:58 -0600

    Versión Inicial Sistema de Reservación Tapanko

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {AppRoutingModule} from "./app-routing.module";
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {LoginComponent} from "./login/login.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
