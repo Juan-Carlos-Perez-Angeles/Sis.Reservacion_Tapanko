bfc1b31a777eba6ebaf7febbba5e21e4972f5464
Juan Carlos Perez Angeles <juan-carlos-perez-@outlook.com>
2022-03-22 10:50:34 -0600

    Direccionamiento a Registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import {Routes,RouterModule} from "@angular/router";
 import {AppComponent} from "./app.component";
 import {LoginComponent} from "./login/login.component";
+import {RegistroComponent} from "./registro/registro.component";
 
 const routes: Routes = [
-  {path: '', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegistroComponent}
 ]
 
 @NgModule({
